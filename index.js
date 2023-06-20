b2d22636cf187b806e430267944d2e62fd0050ff
dannyverap <danny.verap@gmail.com>
2023-06-20 16:16:13 -0500

    deploy

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const PORT = 3001;
+const {
+  PORT
+ } = process.env;
 
 
 conn.sync({ force: false }).then(() =>
